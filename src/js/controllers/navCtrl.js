00d6850be2287e3bba6ed76eb9d1b04005f7239e
seiyria <kyle@seiyria.com>
2015-09-27 09:50:55 -0500

    garbage auth added

--- src/js/controllers/navCtrl.js
+++ src/js/controllers/navCtrl.js
@@ -4,4 +4,23 @@ site.controller('navController', ($scope, $mdSidenav) => {
   $scope.toggleList = () => {
     $mdSidenav('left').toggle();
   };
+
+  $scope.logins = [
+    {
+      name: 'Facebook',
+      icon: 'facebook'
+    },
+    {
+      name: 'Twitter',
+      icon: 'twitter'
+    },
+    {
+      name: 'Google+',
+      icon: 'google-plus'
+    }
+  ];
+
+  $scope.doLogin = (service) => {
+    console.log(service);
+  };
 });
