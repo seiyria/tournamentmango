464cdbeb36d51fdf347c7bbd43e7995216c1a149
Kyle Kemp <kyle@seiyria.com>
2015-09-27 18:57:52 -0500

    fixing build or something

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,2 @@
-import angular from 'angular';
-import 'angular-ui-router';
-import 'angular-material';
-import 'angular-material-icons';
-import 'angular-material-data-table';
-import 'ngstorage';
-import 'angularfire';
 
-export default angular.module('openchallenge', ['ui.router', 'ngMaterial', 'ngMdIcons', 'ngStorage', 'firebase']);
+export default angular.module('openchallenge', ['ngAria', 'ngAnimate', 'ngMaterial', 'ngMdIcons', 'ngStorage', 'ui.router', 'firebase']);
