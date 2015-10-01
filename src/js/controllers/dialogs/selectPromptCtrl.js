115ebae3f6a25f20f2913249526e8c21bf7960f4
seiyria <kyle@seiyria.com>
2015-10-01 13:55:18 -0500

    remove logging

--- src/js/controllers/dialogs/selectPromptCtrl.js
+++ src/js/controllers/dialogs/selectPromptCtrl.js
@@ -12,8 +12,6 @@ site.controller('selectPromptController', ($scope, $mdDialog, title, label, defa
   $scope.selectableValues = _.sortBy(selectableValues);
   $scope.groups = _.uniq(_.pluck(selectableValues, 'group'));
 
-  console.log($scope.selectableValues, $scope.groups);
-
   $scope.submit = () => {
     $scope.form.$setSubmitted();
 
