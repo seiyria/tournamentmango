3c807ecdf531dea1ab51609a2802c19f8d5ce1a0
seiyria <kyle@seiyria.com>
2015-10-01 13:55:07 -0500

    user settings now lets you click-to-copy id, and has download capability

--- src/js/controllers/userSettingsCtrl.js
+++ src/js/controllers/userSettingsCtrl.js
@@ -1,9 +1,26 @@
 import site from '../app';
 
-site.controller('userSettingsController', ($scope, SidebarManagement, EnsureLoggedIn) => {
+site.controller('userSettingsController', ($scope, SidebarManagement, EnsureLoggedIn, FirebaseURL, $firebaseObject, Toaster) => {
 
   SidebarManagement.hasSidebar = true;
   const authData = EnsureLoggedIn.check();
 
   $scope.shareId = btoa(authData.uid);
+
+  const data = $firebaseObject(new Firebase(`${FirebaseURL}/users/${authData.uid}`));
+
+  const clipboard = new Clipboard('#copy-id');
+  clipboard.on('success', () => {
+    Toaster.show(`Copied ID to clipboard!`);
+  });
+
+  $scope.save = () => {
+    const importantData = {
+      events: data.events,
+      players: data.players,
+      tournaments: data.tournaments
+    };
+    const blob = new Blob([JSON.stringify(importantData)], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, `info-${Date.now()}.openchallenge`);
+  };
 });
