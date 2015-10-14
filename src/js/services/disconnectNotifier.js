a991b07e2e8379cdf8d2e2c28d91ff3f5d61dc6d
seiyria <kyle@seiyria.com>
2015-10-14 09:15:29 -0500

    feat(app): app will now ask if you want to leave and are offline
    closes #57

--- src/js/services/disconnectNotifier.js
+++ src/js/services/disconnectNotifier.js
@@ -1,11 +1,13 @@
 import site from '../app';
 
-site.service('DisconnectNotifier', ($q, FirebaseURL) => {
+site.service('DisconnectNotifier', ($q, $window, FirebaseURL) => {
   const firebase = new Firebase(`${FirebaseURL}/.info/connected`);
   const defer = $q.defer();
 
   firebase.on('value', (snap) => {
-    defer.notify(snap.val());
+    const val = snap.val();
+    defer.notify(val);
+    $window.onbeforeunload = val ? undefined : () => 'If you quit this app now, you may lose all unsynced data. Are you sure you want to do this?';
   });
 
   return defer.promise;
