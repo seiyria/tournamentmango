d1416d3a392f9ce81deda858f25bfc4070aee8cf
seiyria <kyle@seiyria.com>
2015-11-23 16:40:32 -0600

    feat(tournaments): tournament headers can now be edited to display custom round titles
    closes #101

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,5 @@
 export default angular.module('openchallenge', [
   'ngAria', 'ngAnimate', 'ngMaterial', 'ngMdIcons', 'ngStorage',
   'ui.router', 'firebase', 'md.data.table', 'angular-sortable-view',
-  'ngSanitize'
+  'ngSanitize', 'angularInlineEdit'
 ]);
