9ab74b7d6c5ecdb1712f6c8bdaaf8a2f69013951
Austin Baldwin <aut.baldwin@gmail.com>
2016-01-04 20:12:17 -0600

    feat(tournaments): Allows tournaments to have no game associated with them if wanted
    closes #109

--- src/js/services/scoring.js
+++ src/js/services/scoring.js
@@ -22,7 +22,7 @@ site.service('ScoringFunctions', (CurrentTournaments, TournamentStatus) => {
         tempScores[player.$id] = 400 * (player.wins - player.losses);
       });
 
-      _.each(_.filter(CurrentTournaments.get(), t => t.status === TournamentStatus.COMPLETED), tournament => {
+      _.each(_.filter(CurrentTournaments.get(), t => t.status === TournamentStatus.COMPLETED && t.game), tournament => {
 
         _.each(tournament.matches, match => {
           if(_.any(match.p, p => p === -1)) return; // ignore walkover matches
